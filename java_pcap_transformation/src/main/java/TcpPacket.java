9f955e44316cf8c08d18a1bbf89b51f35aa8de1a
Kolopack <smgnowis@gmail.com>
2021-07-17 23:24:21 +0200

    Message classes!

---
+++ java_pcap_transformation/src/main/java/TcpPacket.java
@@ -0,0 +1,6 @@
+import java.sql.Timestamp;
+
+public class TcpPacket {
+    private String name;
+    private Timestamp timestamp;
+}
---
+++ java_pcap_transformation/src/main/java/UdpPacket.java
@@ -0,0 +1,7 @@
+public class UdpPacket {
+    private String name;
+    private String sourceIP;
+    private String destinationIP;
+    private int sourcePort;
+    private int destinationPort;
+}
